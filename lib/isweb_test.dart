b916b69df4172598acae41358788583530541cb9
Miguel Angel Besalduch Garcia <mabg.dev@gmail.com>
2024-10-08 15:22:28 -0300

    Upgrade to sdk 3.0.0

--- lib/isweb_test.dart
+++ lib/isweb_test.dart
@@ -1,7 +1,8 @@
-// Copyright 2022 Miguel Angel Besalduch Garcia, mabg.dev@gmail.com. All rights reserved.
+// Copyright 2022 Miguel Angel Besalduch Garcia, mabg.dev@gmail.com.
+// All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-library isweb_test;
+library;
 
 export 'src/isweb_test.dart' show debugIsWeb, isWeb;
